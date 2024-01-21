b8576ef23065c13b5f43426f8e5f2e8c3b1131ca
lan-vin <114327776+lan-vin@users.noreply.github.com>
2024-01-21 15:09:21 +0800

    Update WishService.java

--- Wish/demo/src/main/java/com/example/demo/Service/WishService.java
+++ Wish/demo/src/main/java/com/example/demo/Service/WishService.java
@@ -2,6 +2,6 @@ package com.example.demo.Service;
 import Common.R;
 import com.example.demo.Model.request.WishRequest;
 public interface WishService {
-    R addWishService(WishRequest wishRequest);
-    R getAllWishService(String targetSex);
+    R add(WishRequest wishRequest);
+    R getOne(WishRequest wishRequest, HttpSession session);
 }
